c2b7642e7c03b102868c12c811c40039d830d3c0
leo-caztro <leo-caztro@PC>
2022-07-27 15:25:20 -0400

    15:25 27/07/22

--- sitioweb/sitioweb/Controllers/CrudController.cs
+++ sitioweb/sitioweb/Controllers/CrudController.cs
@@ -10,6 +10,54 @@ namespace sitioweb.Controllers
         {
             return View();
         }
+        public IActionResult Actualizar(int id)
+        {
+            return View(id);
+        }
+
+        //public IActionResult Actualizar()
+        //{
+
+        //    //MySqlDataReader? reader = null;
+        //    //string sql = "SELECT id, nombre, descripcion, precio FROM semilla WHERE codigo LIKE '" + id + "' LIMIT 1";
+        //    //MySqlConnection conexionBD = Conexion.Connexion();
+        //    //conexionBD.Open();
+        //    //try
+        //    //{
+        //    //    MySqlCommand comando = new MySqlCommand(sql, conexionBD);
+        //    //    reader = comando.ExecuteReader();
+        //    //    semilla row = null;
+        //    //    if (reader.HasRows)
+        //    //    {
+        //    //        while (reader.Read())
+        //    //        {
+        //    //            row = new semilla();
+
+        //    //            row.Id = int.Parse(reader.GetString(0));
+        //    //            row.Nombre = reader.GetString(1);
+        //    //            row.Descripcion = reader.GetString(2);
+        //    //            row.Precio = int.Parse(reader.GetString(3));
+        //    //            return View(row);
+        //    //        }
+        //    //    }
+        //    //    else
+        //    //    {
+        //    //        Console.WriteLine("No se encontraron registros");
+        //    //        return RedirectToAction("Index", "Crud");
+        //    //    }
+        //    //}
+        //    //catch (MySqlException execp)
+        //    //{
+        //    //    Console.WriteLine("error al buscar " + execp.Message);
+        //    //    return RedirectToAction("Index", "Crud");
+        //    //}
+        //    //finally
+        //    //{
+        //    //    conexionBD.Close();
+        //    //}
+        //    return View();
+        //}
+
         [HttpPost]
         public IActionResult ingresar(int cod, String nombre, String descrip, int precio)
         {
@@ -59,28 +107,6 @@ namespace sitioweb.Controllers
 
             return RedirectToAction("Index", "Crud");
         }
-        public IActionResult Buscar(int id)
-        {
-            string sql = "UPDATE semilla SET id ='" + cod + "', nombre ='" + nombre + "', descripcion ='" + descrip + "', precio ='" + precio + "'";
-            MySqlConnection conexionDB = Conexion.Connexion();
-            conexionDB.Open();
-            try
-            {
-                MySqlCommand cmd = new(sql, conexionDB);
-                cmd.ExecuteNonQuery();
-            }
-            catch (Exception execp)
-            {
-                Console.WriteLine("ERROR AL ACTUALIZAR " + execp.Message);
-            }
-            finally
-            {
-                conexionDB.Close();
-            }
-
-            return RedirectToAction("actualizar", "Crud");
-        }
-
         public IActionResult update(int cod, String nombre, String descrip, int precio)
         {
             string sql = "UPDATE semilla SET id ='" + cod + "', nombre ='" + nombre + "', descripcion ='" + descrip + "', precio ='" + precio + "'";
